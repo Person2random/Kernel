332eb5a19df75ba162402fccd3790678dbd47d61
Person2random <aser.alnaggar@icloud.com>
2026-02-03 02:02:38 +0200

    Forgot to stage Makefie and kernel.c

--- Makefile
+++ Makefile
@@ -1,13 +1,22 @@
-default: clean build run clean
+default: 
+	make clean
+	make build
+	make run
+	make clean
 
 build:
 	i686-elf-as boot.s -o boot.o
 	i686-elf-as -32 gdtflush.s -o gdtflush.o
+	i686-elf-as -32 idtload.s -o loadidt.o
+	i686-elf-as -32 isr0.s -o isr0.o
+
 	i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 	i686-elf-gcc -c vga.c -o terminal.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 	i686-elf-gcc -c gdt.c -o gdt.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
 	i686-elf-gcc -T linker.ld -o myos -ffreestanding -O2 -nostdlib \
-	boot.o kernel.o terminal.o gdt.o gdtflush.o -lgcc
+		boot.o kernel.o terminal.o gdt.o gdtflush.o loadidt.o isr0.o -lgcc
+
 	mkdir -p isodir/boot/grub
 	cp myos isodir/boot/myos
 	cp grub.cfg isodir/boot/grub/grub.cfg
@@ -16,4 +25,4 @@ build:
 run:
 	qemu-system-i386 -kernel myos
 clean:
-	rm -rf boot.o isodir kernel.o myos myos.iso	terminal.o gdt.o gdtflush.o
+	rm -rf boot.o isodir kernel.o myos myos.iso	terminal.o gdt.o gdtflush.o loadidt.o isr0.o
--- kernel.c
+++ kernel.c
@@ -21,14 +21,73 @@ void* memcpy(void* dst, const void* src, size_t n) {
 }
 
 
+
+//Will add IDT implementation
+
+//TODO: Put IDT in different .h and .c files to avoid clutter
+struct idt_entry {
+    uint16_t offset_low;   // lower 16 bits of handler address
+    uint16_t selector;     // kernel code segment selector
+    uint8_t  zero;         // always 0
+    uint8_t  type_attr;    // flags
+    uint16_t offset_high;  // upper 16 bits of handler address
+} __attribute__((packed));
+
+
+struct idt_ptr {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+#define IDT_ENTRIES 256
+
+struct idt_entry idt[IDT_ENTRIES];
+struct idt_ptr idtp;
+extern void idt_load(uint32_t);
+extern void isr0(void);
+
+//Function to set one entry
+void idt_set_gate(uint8_t num, uint32_t base, uint16_t selector, uint8_t flags) {
+    idt[num].offset_low  = base & 0xFFFF;
+    idt[num].selector    = selector;
+    idt[num].zero        = 0;
+    idt[num].type_attr   = flags;
+    idt[num].offset_high = (base >> 16) & 0xFFFF;
+}
+
+
+
+void idt_init() {
+    idtp.limit = sizeof(idt) - 1;
+    idtp.base  = (uint32_t)&idt;
+
+    // clear table
+    for (int i = 0; i < IDT_ENTRIES; i++) {
+        idt_set_gate(i, 0, 0, 0);
+    }
+    idt_set_gate(0, (uint32_t)isr0, 0x08, 0x8E);
+
+    idt_load((uint32_t)&idtp);
+}
+
+
+
+
+
+
+
+
 uint8_t kernel_stack[16384];
 extern uint8_t kernel_stack[];
-
 void kernel_main(void){
     terminal_initialize();
     terminal_writestring("Will init GDT\n");
     gdt_init();
     terminal_writestring("GDT initialized\n");
+    terminal_writestring("Will init IDT\n");
+    idt_init();
+    terminal_writestring("IDT initialized\n");
+    
     
     while (1)
     {
